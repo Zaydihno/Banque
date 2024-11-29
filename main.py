f42e21cc31b5fd04f1f483dc1497a5cd2173530e
Masmoun007 <altmasmoun@gmail.com>
2024-11-29 18:23:02 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+quit = 1
+
+def afficherMenu():
+    choix = input("""
+    1:Pepsi  3.5$
+    2:Coca    2.5$
+    3:snickers   4$
+    4:Mars   2.5$
+    q:quitter
+    ----
+    """)
+    return choix
+
+while quit == 1:
+    choix = afficherMenu()
+    if choix == 'q':
+        quit = 0
+    if choix == '1':
+        input("quel est votre moyen de paiement?                  carte de credit:1      espece :2")
+        if  input("1")
+        print(inserer votre carte)
+        else print("insérer l'argent")
+        print("paiement reussi✓")  
+        print("voici votre pessi cola bien frais chacal zayd prime")
+    if choix == '2'
+       input("quel est votre moyen de paiement?                  carte de credit:1      espece :2")
+        if  input("1")
+        print(inserer votre carte)
+        else print("insérer l'argent")
+        print("paiement reussi✓")  
+        print("coca bien frais bsahtek")    
+    if choix == '3'
+       input("quel est votre moyen de paiement?                  carte de credit:1      espece :2")
+        if  input("1")
+        print(inserer votre carte)
+        else print("insérer l'argent")
+        print("paiement reussi✓")  
+        print('voici votre snickers')
+    if choix == '4'
+       input("quel est votre moyen de paiement?                  carte de credit:1      espece :2")
+        if  input("1")
+        print(inserer votre carte)
+        else print("insérer l'argent")
+        print("paiement reussi✓")  
+        print('voici votre mars')
